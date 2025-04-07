f4919ca4a93e9f825f8538c2857363d46a35f8e7
skyloved <w2783858980@outlook.com>
2025-04-07 08:20:18 +0800

    实现请求转发功能：修改LoginServlet以使用请求转发代替重定向，增强用户体验

--- src/main/servlet/LoginServlet.java
+++ src/main/servlet/LoginServlet.java
@@ -19,23 +19,34 @@ public class LoginServlet extends HttpServlet {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
         var usersDao = new UsersDao();
-        try {
-            usersDao.findByNameAndPassword(username, password).ifPresentOrElse(user -> {
-                try {
-                    resp.sendRedirect("welcome.html");
-                } catch (IOException e) {
-                    System.out.println(e.getMessage());
-                }
-            },
-                    () -> {
-                        try {
-                            resp.sendRedirect("login.html");
-                            System.out.println("用户名或密码错误");
-                        }
-                        catch (IOException e) {
-                            System.out.println("跳转失败"+e.getMessage());
-                        }
-                    });
+//        try {
+//            usersDao.findByNameAndPass word(username, password).ifPresentOrElse(user -> {
+//                try {
+//                    resp.sendRedirect("welcome.html");
+//                } catch (IOException e) {
+//                    System.out.println(e.getMessage());
+//                }
+//            },
+//                    () -> {
+//                        try {
+//                            resp.sendRedirect("login.html");
+//                            System.out.println("用户名或密码错误");
+//                        }
+//                        catch (IOException e) {
+//                            System.out.println("跳转失败"+e.getMessage());
+//                        }
+//                    });
+//        } catch (SQLException e) {
+//            System.out.println(e.getMessage());
+//        }
+        //使用请求转发
+        try{
+            if (usersDao.findByNameAndPassword(username, password).isPresent()) {
+                req.setAttribute("username", username);
+                req.getRequestDispatcher("welcome.html").forward(req, resp);
+            } else {
+                req.getRequestDispatcher("login.html").forward(req, resp);
+            }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
