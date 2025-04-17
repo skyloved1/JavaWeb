4121fa92d1ab8793fc98753440687e28bd0dc915
skyloved <w2783858980@outlook.com>
2025-04-17 15:19:16 +0800

    实现Cookie功能：更新CreateCookieDemoServlet以创建登录时间Cookie，修改GetCookieDemoServlet以显示Cookie信息并处理登录状态

--- src/test/CreateCookieDemoServlet.java
+++ src/test/CreateCookieDemoServlet.java
@@ -7,16 +7,30 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CreateCookieDemoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        var cookie1=new Cookie("userName","admin");
+        resp.setContentType("text/html;charset=utf-8");
+        resp.setCharacterEncoding("utf-8");
+
+        var cookie1 = new Cookie("userName", "admin");
         var cookie2 = new Cookie("userPassword", "admin");
+        var lastLoginDate = new Cookie(
+                "lastLoginDate",
+                URLEncoder.encode(new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss").format(new Date()), StandardCharsets.UTF_8)
+        );
+
         cookie1.setMaxAge(3600);
         cookie2.setMaxAge(3600);
+        lastLoginDate.setMaxAge(3600);
+
         resp.addCookie(cookie1);
         resp.addCookie(cookie2);
-
+        resp.addCookie(lastLoginDate);
     }
 }
--- src/test/GetCookieDemoServlet.java
+++ src/test/GetCookieDemoServlet.java
@@ -6,15 +6,32 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 public class GetCookieDemoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        var cookies=req.getCookies();
-        var writer=resp.getWriter();
-        resp.setContentType("text/html,utf-8");
-        for (var cookie:cookies){
-            writer.println(cookie.getName()+"="+cookie.getValue()+"\n");
+        req.setCharacterEncoding("utf-8");
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8"); // 修复内容类型
+
+        var cookies = req.getCookies();
+        var writer = resp.getWriter();
+        int hasLogin = -1;
+        int index = 0;
+        for (var cookie : cookies) {
+            if (cookie.getName().equals("lastLoginDate")) {
+                hasLogin = index;
+                continue;
+            }
+            writer.println(cookie.getName() + "=" + cookie.getValue() + "\n");
+            index++;
+        }
+        if (hasLogin == -1) {
+            writer.println("没有登录过");
+        } else {
+            writer.println("欢迎回来,上次登录时间：" + URLDecoder.decode(cookies[hasLogin].getValue(), StandardCharsets.UTF_8));
         }
     }
 }
